c2e9e8c78ce559fac39de5e0a1e374feb7e6b677
Tushar Singh <singh772@purdue.edu>
2022-06-16 19:37:28 +0000

    Modified version of the Portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,51 +1,39 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random fun fact about myself to the page.
- */
-function addRandomFact() {
-    const facts = [
-      "I love anything matcha flavored.",
-      "My name has the meaning of sunrise and dawn.",
-      "I like Marvel Movies.",
-      "I am trying to learn ice skating.",
-      "My favorite anime is The Promised Neverland.",
-    ];
+  const handleFirstTab = (e) => {
+    if(e.key === 'Tab') {
+      document.body.classList.add('user-is-tabbing')
   
-    // Pick a random greeting.
-    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-    // Add it to the page.
-    const greetingContainer = document.getElementById('greeting-container');
-    greetingContainer.innerText = greeting;
-
+      window.removeEventListener('keydown', handleFirstTab)
+      window.addEventListener('mousedown', handleMouseDownOnce)
+    }
+  
   }
   
-  /**
-   * Gets messages as json from hello endpoint. Randomly select one.
-   */
-  async function getMessage() {
-    const res = await fetch("/hello");
-    const message = await res.json();
-    const oneMessage = message[Math.floor(Math.random() * message.length)];
-    document.getElementById("message-container").innerText = oneMessage;
+  const handleMouseDownOnce = () => {
+    document.body.classList.remove('user-is-tabbing')
+  
+    window.removeEventListener('mousedown', handleMouseDownOnce)
+    window.addEventListener('keydown', handleFirstTab)
   }
   
-  /**
-   * Dark Mode Toggle Control
-   */
-  document.getElementById("dark-mode-toggle").onclick = function (e) {
-    darkmode.toggleDarkMode();
+  window.addEventListener('keydown', handleFirstTab)
+  
+  const backToTopButton = document.querySelector(".back-to-top");
+  let isBackToTopRendered = false;
+  
+  let alterStyles = (isBackToTopRendered) => {
+    backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+    backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+    backToTopButton.style.transform = isBackToTopRendered
+      ? "scale(1)"
+      : "scale(0)";
   };
+  
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 700) {
+      isBackToTopRendered = true;
+      alterStyles(isBackToTopRendered);
+    } else {
+      isBackToTopRendered = false;
+      alterStyles(isBackToTopRendered);
+    }
+  });
